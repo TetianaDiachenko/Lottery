3f378a2053df6a3d399f1a64860868deb49c0d1b
TetianaDiachenko <tetianamyhun@gmail.com>
2024-03-31 16:42:32 +0300

    create a project

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+// export let configFTP = {
+// 	host: "", //Address of FTP server
+// 	user: "if0_35800612", // User's name
+// 	password: "jfBlse4qbpdyP", // Password
+// 	parallel: 5 // Number of simultaneous threads
+// }
---
+++ src/js/app.js
@@ -0,0 +1,50 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+// Popup
+const popupLinks = document.querySelectorAll('.popup-link');
+
+let unlock = true;
+
+if(popupLinks.length > 0) {
+	for (let index = 0; index < popupLinks.length; index++) {
+		const popupLink = popupLinks[index];
+		popupLink.addEventListener('click', function(e) {
+			const popupName = popupLink.getAttribute('href').replace('#', '');
+			const curentPopup = document.getElementById(popupName);
+			popupOpen(curentPopup);
+			e.preventDefault();
+		});
+	}
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
+	for(let index = 0; index < popupCloseIcon.length; index++) {
+		const el = popupCloseIcon[index];
+		el.addEventListener('click', function (e) {
+			popupClose(el.closest('.popup'));
+			e.preventDefault();
+		});
+	}
+}
+
+function popupOpen(curentPopup) {
+	if(curentPopup && unlock) {
+		curentPopup.classList.add('open');
+		curentPopup.addEventListener('click', function(e) {
+			if(!e.target.closest('.popup__content')) {
+				popupClose(e.target.closest('.popup'));
+			}
+		});
+	}
+}
+
+function popupClose(popupActive) {
+	if (unlock) {
+		popupActive.classList.remove('open');
+	}
+}
+
+
